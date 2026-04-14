210883e14705a5ca701d50a3175d787f742f7a50
Ruan Belmiro <ruan.carlos.belmiro@gmail.com>
2026-04-14 17:32:08 -0300

    chore: setup inicial do projeto com React + Vite + TypeScript + AntDesign

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <section id="center">
+        <div>
+          <p>Hello Vite!</p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
